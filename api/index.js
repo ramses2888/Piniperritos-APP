4f3426e66d021e1dd906265feaf366f26da69da6
ramses2888 <ramsesarnold2888@gmail.com>
2022-02-28 19:56:42 -0500

    update files models, and charger

--- api/index.js
+++ api/index.js
@@ -25,6 +25,6 @@ const { conn } = require("./src/db.js");
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
-    charger();
+    charger()
   });
 });
